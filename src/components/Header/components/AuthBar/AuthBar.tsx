d3939be2402ebaab3bf7649969025d3dec31e354
andreypotkas <potkasa@gmail.com>
2023-11-25 23:50:20 +0300

    add user icon

--- src/components/Header/components/AuthBar/AuthBar.tsx
+++ src/components/Header/components/AuthBar/AuthBar.tsx
@@ -3,15 +3,17 @@ import ThemeSwitcher from "../ThemeSwitcher/ThemeSwitcher";
 
 import "./AuthBar.scss";
 import MemoizedAuthBtn from "./components/AuthBtn/AuthBtn";
+import MemoizedUserIcon from "./components/UserIcon/UserIcon";
 
 function InfoLine() {
   return (
-    <div className="info-line">
+    <div className="auth-bar">
       <div className="flex gap-2 text-3xl text-primary">Run connect</div>
       <div
-        className="flex gap-2 flex-shrink-0 overflow-hidden p-1"
+        className="flex gap-2 flex-shrink-0 p-1"
         style={{ background: "var(--surface-card)" }}
       >
+        <MemoizedUserIcon />
         <ThemeSwitcher />
         <MemoizedAuthBtn />
       </div>
---
+++ src/components/Header/components/AuthBar/components/UserIcon/UserIcon.tsx
@@ -0,0 +1,50 @@
+import { Avatar } from "primereact/avatar";
+import { Badge } from "primereact/badge";
+import { Menu } from "primereact/menu";
+import { MenuItem } from "primereact/menuitem";
+import { OverlayPanel } from "primereact/overlaypanel";
+import React, { useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import avatar from "@/assets/images/avatar.png";
+
+function UserIcon() {
+  const navigate = useNavigate();
+  const op = useRef<OverlayPanel | null>(null);
+
+  const items: MenuItem[] = [
+    {
+      label: "Настройки",
+      icon: "pi pi-cog",
+      command: () => navigate("/settings"),
+    },
+    {
+      label: "Уведомления",
+      icon: "pi pi-bell",
+      command: () => navigate("/settings"),
+    },
+    {
+      label: "Личный кабинет",
+      icon: "pi pi-user",
+      command: () => navigate("/settings"),
+    },
+  ];
+
+  return (
+    <div>
+      <Avatar
+        className="p-overlay-badge "
+        image={avatar}
+        size="large"
+        onClick={(e) => op.current!.toggle(e)}
+      >
+        <Badge value="4" severity="danger" />
+      </Avatar>
+      <OverlayPanel ref={op}>
+        <Menu model={items} />
+      </OverlayPanel>
+    </div>
+  );
+}
+
+const MemoizedUserIcon = React.memo(UserIcon);
+export default MemoizedUserIcon;
